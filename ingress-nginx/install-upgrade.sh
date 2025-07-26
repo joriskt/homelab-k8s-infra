77fffc26973de20d1b2a8697f617de09365b62dd
Joris Klein Tijssink <noreply@joris.me>
2025-07-26 12:49:32 +0200

    Move ingress-nginx/ to homelab-k8s-apps repository

--- ingress-nginx/install-upgrade.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-helm upgrade \
-    --install ingress-nginx \
-    --repo https://kubernetes.github.io/ingress-nginx \
-    --namespace ingress-nginx \
-    --create-namespace \
-    ingress-nginx
--- ingress-nginx/watch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Command obtained from the Helm install.
-# It said to run this to monitor the controller availability.
-kubectl get service \
-    --namespace ingress-nginx \
-    ingress-nginx-controller \
-    --output wide \
-    --watch
